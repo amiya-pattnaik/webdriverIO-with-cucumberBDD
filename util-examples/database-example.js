77d3be16bc58a0479c4083e75e7add136ce05c4e
pattnaik_amiya <pattnaik_amiya@yahoo.com>
2018-03-02 22:26:36 -0600

    updated

--- util-examples/database-example.js
+++ util-examples/database-example.js
@@ -1,11 +1,12 @@
 
-var db        = require('node-any-jdbc');
-var dbc       = require('./db');
-const assert  = require('assert');
-
-
-var sql = 'SELECT * FROM emp_info where emp_name = "Amiya"';
-db.execute(dbc.mysql, sql, function(results){
-  console.log(results);
-
-});
+// var db        = require('node-any-jdbc');
+// var dbc       = require('./db');
+// const assert  = require('assert');
+//
+//
+// var sql = 'SELECT * FROM emp_info where emp_name = "Amiya"';
+// db.execute(dbc.mysql, sql, function(results){
+//   console.log(results);
+//   //then do what ever validation you to do withe results
+//
+// });
